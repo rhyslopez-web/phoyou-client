4ec07ea6676d0c8606b25056f3dbb3b1e4df3805
rhyslopez-web <rhyslopez.web@gmail.com>
2025-10-06 22:00:58 -0700

    fixed buiild issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "./components/Hero/Hero";
 import HomeAbout from "./components/HomeAbout/HomeAbout";
 import HomeMenu from "./components/HomeMenu/HomeMenu";
 import DividerBottom from "./components/DividerBottom/DividerBottom";
-import DividerTop from "./components/DividerTop/DividerTop";
 import HomeTrustIcons from "./components/HomeTrustIcons/HomeTrustIcons";
 import HomeReviews from "./components/HomeReviews/HomeReviews";
 import HomeGallery from "./components/HomeGallery/HomeGallery";
